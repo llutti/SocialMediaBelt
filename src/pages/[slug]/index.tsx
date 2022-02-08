c6f9d4e6523f7b6c185d2f63534da483bc754c33
llutti <llutti@outlook.com>
2022-02-08 15:16:03 -0300

    Start dev

---
+++ src/pages/[slug]/index.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from 'next';
+
+const TenantHome: NextPage = () =>{
+  return (
+    <>
+      <h1>Tenant Home</h1>
+    </>
+  );
+}
+
+export default TenantHome;
---
+++ src/pages/index.tsx
@@ -0,0 +1,26 @@
+import type { NextPage } from 'next'
+
+import Link from 'next/link'
+
+import Seo from '@components/Seo'
+
+
+const Home: NextPage = () =>
+{
+  return (
+    <div className={''}>
+
+      <Seo />
+
+      <h1 className='font-bold text-4xl'>HOME</h1>
+
+
+      <ul>
+        <li><Link href='/app'><a>App</a></Link></li>
+        <li><Link href='/llutti'><a>Tenant Llutti</a></Link></li>
+      </ul>
+    </div>
+  )
+}
+
+export default Home
