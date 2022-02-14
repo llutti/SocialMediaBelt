af560eff9f91fa32e1a60834a2eaddc2bc91f8b5
llutti <llutti@outlook.com>
2022-02-14 07:58:54 -0300

    Aula 5

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+import { prisma } from "@lib/prisma";
+
 type Data = {
-  name: string
+  id: number,
+  name: string,
+  slug: string
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+  res: NextApiResponse<Data[]>
+)
+{
+  const accounts = await prisma?.account.findMany();
+  res.status(200).json(accounts ?? [])
 }
