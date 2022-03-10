c29f4323e87de8bd080cdf25e8e785833fcae4b9
llutti <llutti@outlook.com>
2022-03-10 16:22:08 -0300

    Formatar a identidade visual

--- src/pages/[slug]/index.tsx
+++ src/pages/[slug]/index.tsx
@@ -1,13 +1,50 @@
 import { GetServerSideProps, NextPage } from 'next';
 
 import { findTenantBySlug } from '@services/tenants';
+import { getPublicLinks, LinkPublicType } from '@services/links';
+import Head from 'next/head';
 
-const TenantHome: NextPage = (props) =>
+interface Props
+{
+  tenant: { id: string, name: string };
+  links: LinkPublicType[];
+}
+
+const TenantHome: NextPage<Props> = ({ tenant, links, ...props }) =>
 {
   return (
     <>
-      <h1>Tenant Home</h1>
-      {JSON.stringify(props, null, 2)}
+      <Head>
+        <title>{tenant.name} - Social Media Belt</title>
+        <meta name='description' content="Todos os link's do LLUTTI's Tenant"/>
+      </Head>
+
+      <div className='max-w-xl mx-auto pt-4'>
+        <h1 className='text-center font-bold text-4xl'>{tenant.name}</h1>
+        {
+          links.map(
+            link =>
+            {
+              return (
+                <a
+                  key={link.id}
+                  href={link.destination}
+                  className='bg-gray-800 text-white py-4 w-full block text-center hover:bg-gray-200 hover:text-gray-800 border-2 border-solid my-4 transition-all'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  {link.publicName}
+                </a>
+              )
+            }
+          )
+        }
+
+        <hr />
+        <footer className='text-center text-sm mt-2'>
+          Construído com Social Media Belt
+        </footer>
+      </div>
     </>
   );
 }
@@ -21,21 +58,22 @@ export const getServerSideProps: GetServerSideProps = async (context) =>
   if (slug?.indexOf('.') === -1)
   {
     tenant = await findTenantBySlug(String(context?.params?.slug));
-    if (!tenant)
-    {
-      return {
-        notFound: true,
-      }
+  }
+
+  if (!tenant)
+  {
+    return {
+      notFound: true,
     }
   }
 
-
-
+  const links = await getPublicLinks(tenant?.id);
 
   return {
     props: {
       ...context?.params,
       tenant,
+      links,
     }
   }
 }
